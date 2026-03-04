6423bb0c15d24133ed245a88d351611efd8fe45e
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-03-04 19:29:34 +0300

    feat: main router defination

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,37 +1,26 @@
+import { Router } from "../router/router";
 import { Server } from "./server";
 import { IncomingMessage, ServerResponse } from "http";
 
 type Handler = (req: IncomingMessage, res: ServerResponse) => void;
 
-interface Route {
-  method: string;
-  path: string;
-  handler: Handler;
-}
-
 export class Application {
-  private routes: Route[] = [];
-
-  get(path: string, handler: Handler) {
-    this.routes.push({ method: "GET", path, handler });
+  private router: Router;
+  constructor() {
+    this.router = new Router();
   }
 
-  post(path: string, handler: Handler) {
-    this.routes.push({ method: "POST", path, handler });
+  get(path: string, handler: any) {
+    this.router.add("GET", path, handler);
+  }
+
+  post(path: string, handler: any) {
+    this.router.add("POST", path, handler);
   }
 
   listen(port: number, callback?: () => void) {
     const server = new Server((req, res) => {
-      const route = this.routes.find(
-        r => r.method === req.method && r.path === req.url
-      );
-
-      if (route) {
-        route.handler(req, res);
-      } else {
-        res.statusCode = 404;
-        res.end("Not Found");
-      }
+      this.router.handle(req, res);
     });
 
     server.listen(port, callback);
